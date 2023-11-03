65abcafac2b8fc7f4a39dd5198dd203f3c3f18d5
Luna Joffily <luna.joffily@gmail.com>
2023-11-02 23:50:46 -0400

    Finished project with error

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Let_me_generate
-Password Generator 
+
+## Description
+
+This webpage was created to generate random passwords for the users and give them a unique password every time. I build this project to create a save password that is unique and that will help users prevent being hacked to. 
+- What did you learn?
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
--- assets/script.js
+++ assets/script.js
@@ -1,95 +1,73 @@
-var passwordText = document.querySelector("#password");
-
+var passwordText = document.querySelector('#password');
+var generateClick = document.querySelector('#generate');
 
 var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
 var numberSelector = '0123456789';
 var symbolSelector = '{}#,!_@/();:.|%$=+-*[]^?&~';
-var lengthCharacter = upperLetters, lowerLetters, numberSelector, symbolSelector
+var passwordGenerater = [''];
 
-var userOpt = function() {
-  
-  
-}
-
-
-function generatePassword(allElement) {
-	
-  userChooseupperLetters = prompt("Would you like to include uppercase characters? Please reply with yes or no.");
-  if (userChooseupperLetters === "yes") {
-    alert("We will uppercase Characters!");
-    userChooseupperLetters = true;
-  } else if (userChooseupperLetters !== "yes") {
-    alert("We will not be adding uppercase Characters!");
-    userChooseupperLetters = false;
+var passwordQuestions = function () {
+  var upperQuestion = confirm('Would you like upper case letters?');
+  if (upperQuestion) {
+    alert('Your password will have upper case letters.');
+  } else {
+    alert('Your password will not have upper case letters.')
   }
-  
-  userChooselowerLetters = prompt("Would you like to include lowercase characters? Please reply with yes or no.")
-  if (userChooselowerLetters === "yes") {
-    alert("We will lowercase Characters!");
-    userChooselowerLetters = true;
-  } else if (userChooselowerLetters !== "yes") {
-    alert("We will not be adding lowercase Characters!");
-    userChooselowerLetters = false;
+  if (upperQuestion) {
+    passwordGenerater = passwordGenerater.concat(upperLetters);
   }
-  
-  userChoosenumberSelector = prompt("Would you like to include number characters? Please reply with yes or no.")
-  if (userChoosenumberSelector === "yes") {
-    alert("We will numbers Characters!");
-    userChoosenumberSelector = true;
-  } else if (userChoosenumberSelector !== "yes") {
-    alert("We will not be adding number Characters!");
-    userChoosenumberSelector = false;
-}
 
-userChoosesymbolSelector = prompt("Would you like to include symbol characters? Please reply with yes or no.")
-  if (userChoosesymbolSelector === "yes") {
-    alert("We will symbol Characters!");
-    userChoosesymbolSelector = true;
-  } else if (userChoosesymbolSelector !== "yes") {
-    alert("We will not be adding symbol Characters!");
-    userChoosesymbolSelector = false;
-}
-
-userChooselengthChar = prompt("How long would you like your passwored to be? Choose from 8 to 128 characters.");
-if (userChooselengthChar < 8 || userChooselengthChar > 128) {
-  alert("It needs to be between 8 and 128!");
-  userChooselengthChar = true;
-} else if (userChooselengthChar !== "yes") {
-  alert("You have choosen " + userChooselengthChar);
-  userChooselengthChar = false;
-}
-
-}
-
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
- passwordText.value = password;
-
- function userPassword() {
-  var result = "";
-  var length = userChooselengthChar;
-  for (i = 0; i < length; i++) {
-    lengthSelector(Math.floor(Math.random() * userChooselengthChar))
+  var lowerQuestion = confirm('Would you like lower case letters?');
+  if (lowerQuestion) {
+    alert('Your password will have lower case letters.');
+  } else {
+    alert('Your password will not have lower case letters.')
   }
-  if (userChooseupperLetters === true) {
-upperLetters[Math.floor(Math.random() * upperLetters.lenght)];
-}
-  if (userChooselowerLetters === true) {
-lowerLetters[Math.floor(Math.random() * lowerLetters.lenght)];
-}
-  if (userChoosenumberSelector === true) {
-numberSelector[Math.floor(Math.random() * numberSelector.lenght)];
-}
-  if (userChoosesymbolSelector === true) {
-symbolSelector[Math.floor(Math.random() * symbolSelector.lenght)];
+  if (lowerQuestion) {
+    passwordGenerater = passwordGenerater.concat(lowerLetters);
+  }
+
+  var numberQuestion = confirm('Would you like numbers?');
+  if (numberQuestion) {
+    alert('Your password will have numbers.');
+  } else {
+    alert('Your password will not have numbers.')
+  }
+  if (numberQuestion) {
+    passwordGenerater = passwordGenerater.concat(numberSelector);
+  }
+
+  var symbolQuestion = confirm('Would you like symbols?');
+  if (symbolQuestion) {
+    alert('Your password will have symbol.');
+  } else {
+    alert('Your password will not have symbol.')
+  }
+  if (symbolQuestion) {
+    passwordGenerater = passwordGenerater.concat(symbolSelector);
+  }
+
+  numberCharactersQuestion = prompt('How many characters would you like in your password?\nYour choose needs to be at least 8 characters and no more then 128 characters');
+  if (numberCharactersQuestion > 8 || numberCharactersQuestion < 128) {
+    alert('You have choosen a input of ' + numberCharactersQuestion);
+  } else if  (numberCharactersQuestion < 8 || numberCharactersQuestion > 128) {
+    alert('You have choosen a unvalid input! Please use a valid input.');
+    return;
+  } else {
+    alert('You have not choosen a input! Please use a valid input.');
+  }
+  for (i = 0; i < numberCharactersQuestion; i++) {
+    let randomChoose = [Math.floor(Math.random() * numberCharactersQuestion.length)];
+    generatedPassword = '';
+    generatedPassword = generatedPassword + numberCharactersQuestion[randomChoose];
+  }
+  return generatedPassword;
 }
 
-return userPassword
-}
+function displayPassword() {
+  var password = passwordQuestions();
+  passwordText.value = password;
 }
 
-generateBtn.addEventListener("click", writePassword); 
+generateClick.addEventListener('click', displayPassword)
